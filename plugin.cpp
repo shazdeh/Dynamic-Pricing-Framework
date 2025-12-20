d041935a420d75a8779866cd87a8ae0a99708cbe
Shazdeh <hassan.derakhshandeh@gmail.com>
2025-12-20 07:56:38 +0330

    progress

--- plugin.cpp
+++ plugin.cpp
@@ -2,15 +2,52 @@
     std::vector<std::string> keywords;
     BGSPerk* perk;
     std::vector<BGSLocation*> locations;
+    std::vector<TESFaction*> vendorFactions;
     TESGlobal* global;
     float gloablVal = 1.0f;
+    TESQuest* completedQuest;
     float buyMult = 1.0f;
     float sellMult = 1.0f;
 };
 
 std::vector<Rule> rules;
 TESGlobal* disabled;
-Actor* player;
+PlayerCharacter* player;
+
+bool PlayerIsInLocation(BGSLocation* location) {
+    BGSLocation* current = player->GetCurrentLocation();
+    while (current) {
+        if (current == location) return true;
+        current = current->parentLoc;
+    }
+    return false;
+}
+
+bool PlayerIsInAnyLocations(std::vector<BGSLocation*> locations) {
+    for (auto location : locations) {
+        if (PlayerIsInLocation(location)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+RE::Actor* GetBarterTarget() {
+    ObjectRefHandle speaker = MenuTopicManager::GetSingleton()->speaker;
+    if (speaker) {
+        return speaker.get().get()->As<Actor>();
+    }
+    return nullptr;
+}
+
+bool IsInAnyFaction(Actor* actor, std::vector<TESFaction*> vendorFactions) {
+    for (auto faction : vendorFactions) {
+        if (actor->IsInFaction(faction)) {
+            return true;
+        }
+    }
+    return false;
+}
 
 bool ValidateRule(Rule& theRule) {
     if (theRule.perk) {
@@ -18,6 +55,17 @@ bool ValidateRule(Rule& theRule) {
     }
     if (theRule.global) {
         if (theRule.global->value != theRule.gloablVal) return false;
+    }
+    if (theRule.locations.size()) {
+        if (!PlayerIsInAnyLocations(theRule.locations)) return false;
+    }
+    if (theRule.vendorFactions.size()) {
+        Actor* target = GetBarterTarget();
+        if (!target) return false;
+        if (!IsInAnyFaction(target, theRule.vendorFactions)) return false;
+    }
+    if (theRule.completedQuest) {
+        if (!theRule.completedQuest->IsCompleted()) return false;
     }
     return true;
 }
@@ -38,11 +86,25 @@ void Inject(BSFixedString menuName) {
 
     GFxValue data;
     movie->CreateArray(&data);
+    bool bInject = false;
     for (auto& rule : rules) {
-        if (ValidateRule(rule)) {
-            
+        if (!ValidateRule(rule)) continue;
+        bInject = true;
+        GFxValue keywords;
+        movie->CreateArray(&keywords);
+        keywords.SetArraySize(rule.keywords.size());
+        for (int i = 0; i < rule.keywords.size(); i++) {
+            keywords.SetElement(i, GFxValue(rule.keywords[i]));
         }
+        GFxValue ruleData;
+        movie->CreateObject(&ruleData);
+        ruleData.SetMember("keywords", keywords);
+        ruleData.SetMember("buy", rule.buyMult);
+        ruleData.SetMember("sell", rule.sellMult);
+        data.PushBack(ruleData);
     }
+    ConsoleLog::GetSingleton()->Print(fmt::format("Injecting: {}", bInject ? "yes" : "nah").c_str());
+    if (!bInject) return;
     _root.SetMember("DPF", data);
 
     GFxValue args[2];
@@ -59,7 +121,7 @@ void Inject(BSFixedString menuName) {
 void ParseData(const json& data) {
     rules.reserve(data.size());
     for (const auto& item : data) {
-        Rule newRule;
+        Rule newRule{};
         if (item.contains("itemKeyword")) {
             const auto& kw = item.at("itemKeyword");
             if (kw.is_string()) {
@@ -72,18 +134,60 @@ void ParseData(const json& data) {
             auto& conditions = item.at("conditions");
             if (conditions.contains("perk")) {
                 newRule.perk = TESForm::LookupByEditorID<BGSPerk>(conditions.at("perk").get<std::string>());
+                if (!newRule.perk) continue;
             }
-            if (conditions.contains("location")) {
+            if (conditions.contains("locations")) {
+                const auto& locations = conditions.at("locations");
+                if (locations.is_string()) {
+                    BGSLocation* form = TESForm::LookupByEditorID<BGSLocation>(locations.get<std::string>());
+                    if (form) {
+                        newRule.locations = {form};
+                    } else {
+                        continue; // don't need to add
+                    }
+                } else if (locations.is_array()) {
+                    for (const auto& location : locations) {
+                        BGSLocation* form = TESForm::LookupByEditorID<BGSLocation>(location.get<std::string>());
+                        if (form) {
+                            newRule.locations.push_back(form);
+                        }
+                    }
+                    if (newRule.locations.empty()) {
+                        continue; // none of the locations were valid, throw it away
+                    }
+                }
+            }
+            if (conditions.contains("vendorFactions")) {
+                const auto& vendorFactions = conditions.at("vendorFactions");
+                if (vendorFactions.is_string()) {
+                    TESFaction* form = TESForm::LookupByEditorID<TESFaction>(vendorFactions.get<std::string>());
+                    if (form) {
+                        newRule.vendorFactions = {form};
+                    } else {
+                        continue;
+                    }
+                } else if (vendorFactions.is_array()) {
+                    for (const auto& faction : vendorFactions) {
+                        TESFaction* form = TESForm::LookupByEditorID<TESFaction>(faction.get<std::string>());
+                        if (form) {
+                            newRule.vendorFactions.push_back(form);
+                        }
+                    }
+                    if (newRule.vendorFactions.empty()) {
+                        continue; // none of the vendorFactions were valid, throw it away
+                    }
+                }
+            }
+            if (conditions.contains("completedQuest")) {
+                newRule.completedQuest = TESForm::LookupByEditorID<TESQuest>(conditions.at("completedQuest").get<std::string>());
+                if (!newRule.completedQuest) continue;
             }
         }
-        if (item.contains("changes")) {
-            auto& changes = item.at("changes");
-            if (changes.contains("buy")) {
-                newRule.buyMult = changes.at("buy").get<float>();
-            }
-            if (changes.contains("sell")) {
-                newRule.sellMult = changes.at("sell").get<float>();
-            }
+        if (item.contains("buyMult")) {
+            newRule.buyMult = item.at("buyMult").get<float>();
+        }
+        if (item.contains("sellMult")) {
+            newRule.sellMult = item.at("sellMult").get<float>();
         }
         rules.emplace_back(std::move(newRule));
     }
@@ -91,19 +195,18 @@ void ParseData(const json& data) {
 
 void BuildRules() {
     const std::filesystem::path dir = "Data/SKSE/Plugins/DynamicPricing";
-    if (!std::filesystem::exists(dir)) {
-        return;
-    }
+    if (!std::filesystem::exists(dir)) return;
     for (auto& file : std::filesystem::directory_iterator(dir)) {
         std::ifstream ifile{file.path()};
         if (!ifile) continue;
-        json data = json::parse(ifile, nullptr, false);
-        if (data.is_discarded()) continue;
-        ParseData(data);
+        try {
+            json data = json::parse(ifile);
+            if (data.is_discarded()) continue;
+            ParseData(data);
+        } catch (...) {
+        }
     }
 }
-
-bool PlayerIsInLocation(BGSLocation* location) { return true; }
 
 bool IsDisabled() { return disabled && disabled->value == 1.0f; }
 
